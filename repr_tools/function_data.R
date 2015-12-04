bd701d9cbc3db24957cd0d17f7255d592f9155c3
Christian-B <github@brenn.co.uk>
2015-12-04 12:37:22 +0000

    Added tool for a function on all the data

---
+++ repr_tools/function_data.R
@@ -0,0 +1,65 @@
+suppressPackageStartupMessages(require(made4))
+suppressPackageStartupMessages(require(impute))
+suppressPackageStartupMessages(require(repr))
+suppressPackageStartupMessages(require(optparse))
+ 
+load_utils <- function(){
+    args <- commandArgs(trailingOnly = TRUE)
+    script_flag_pos = grep ("--script_dir",args)
+    if (length(script_flag_pos) == 0){
+        cat("--script_dir flag missing\n")
+        source("brenninc_util.R")
+    } else {
+        if (args[script_flag_pos] == "--script_dir") {
+            script_dir = args[script_flag_pos+1]
+        } else {
+            script_dir = substring(args[script_flag_pos],14)
+        }
+        source(paste0(c(script_dir,"brenninc_utils.R"), collapse = "/"))
+    }
+}
+ 
+function_options <- function(){
+    new_list = list(
+        make_option("--data_function", action="store", type='character', 
+                    help="Function to apply on all the data. Currently supported funtions are na_per_column, na_per_row")
+    )
+    return (new_list)
+}
+
+check_function  <- function(){ 
+    check_variable("data_function", legal_values=c("na_per_column","na_per_row"))
+}
+
+
+apply_function <- function(data){
+    mymessages(c("Applying function",opt$data_function, "to the data."))
+    if (opt$data_function == "na_per_column"){
+        return (colSums(is.na(data)))
+    } else if (opt$data_function == "na_per_row"){
+        return (rowSums(is.na(data)))
+    } else {
+        myerror(c("Unexpected fuction",opt$data_function))
+    }
+}
+  
+#Main
+if (!exists("main_flag")){
+    main_flag <<- TRUE
+
+    load_utils()
+
+    init_utils (function_options())
+
+    check_function()
+
+    data <- read_the_data(description="Input data")
+
+    data <- apply_function(data)
+
+    #write_the_data (data, paste0("data after ", opt$function, "data", collapse = " "))
+    write_the_data (data, paste0("data after ", opt$data_functio, collapse = " "))
+
+} 
+
+
