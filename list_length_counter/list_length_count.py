4b8f10e5d754ba16ca43c6b1585dc62a54f46223
= <christian.brenninkmeijer@manchester.ac.uk>
2016-01-14 15:26:43 +0000

    Add test to count files in a list with list of names

--- list_length_counter/list_length_count.py
+++ list_length_counter/list_length_count.py
@@ -15,7 +15,7 @@ def appendInt(num):
     else:
         return str(num) + 'th'
 
-def file_names(list_path, files):
+def file_names(list_path, files, start_number):
     names = list()
     if list_path:
         with open(list_path, 'r') as list_file:
@@ -24,28 +24,30 @@ def file_names(list_path, files):
         if len(names) != len(files):
             raise Exception("Length of list " + list_path + " is " + str(len(names)) + " while " + str(len(files)) + " files specified")
     else:
-        for i in range(1,len(files)+1):
+        for i in range(start_number,len(files)+start_number):
             names.append(appendInt(i))
     return names
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option("-l", "--list", action="store", dest="list", type="string",
-                      help="Path to list of files names. If not provided files will be numbered.")
-    parser.add_option("-o", "--output", action="store", dest="output", type="string",
-                      help="Path to output file.")
     parser.add_option("-f", "--file", action="append", dest="files", type="string",
                       help="File to be read. ")
+    parser.add_option("-l", "--list", action="store", dest="list", type="string",
+                      help="Path to list of files names. If not provided files will be numbered.")
+    parser.add_option("-s", "--start_number", action="store", dest="start_number", type="int", default=1,
+                      help="Number to start count with if no list provided.")
+    parser.add_option("-o", "--output", action="store", dest="output", type="string",
+                      help="Path to output file.")
     (options, args) = parser.parse_args()
 
-    names = file_names(options.list, options.files)
+    names = file_names(options.list, options.files, options.start_number)
     print names
 
     with open(options.output, 'w') as output_file:
-        lines = 0
-        chars = 0
         file_number = -1
         for file_path in options.files:
+            lines = 0
+            chars = 0
             file_number+= 1
             with open(file_path, 'r') as in_file:
                 for line in in_file:
