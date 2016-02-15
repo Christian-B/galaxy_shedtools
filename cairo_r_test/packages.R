63e119db06dc21538e591395f5e6449552ef44a1
Christian-B <github@brenn.co.uk>
2016-02-15 15:09:30 +0000

    Removed a lot of ald stuff

--- cairo_r_test/packages.R
+++
@@ -1,5 +0,0 @@
-library(Cairo)
-
-args<-commandArgs(TRUE)
-writeLines(capture.output(sessionInfo()), args[1])
-sessionInfo()
--- r_extend_level_one/packages.R
+++
@@ -1,5 +0,0 @@
-library(Rcpp)
-
-args<-commandArgs(TRUE)
-writeLines(capture.output(sessionInfo()), args[1])
-sessionInfo()
